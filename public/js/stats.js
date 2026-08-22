9974213a6da94f2ecdf9ac4bc8353a9b2ba1554f
spaghetti <spgeti4@gmail.com>
2026-08-22 13:31:05 +0100

    Added online time to statistics section and renamed statistics section to "This site has..."

--- public/js/stats.js
+++ public/js/stats.js
@@ -7,6 +7,35 @@ let domain = "spaghetti.nekoweb.org";
     const request = await fetch(`https://nekoweb.org/api/site/info/${domain}`);
     const json = await request.json();
 
+    const minute = 1000 * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const now = Date.now();
+    const then = 1742495320565;
+    const diff = (now - then);
+    const days = Math.floor(diff / day);
+
+    if (document.getElementById("views"))
+      document.getElementById("views").innerHTML = json.views;
+    if (document.getElementById("followers"))
+      document.getElementById("followers").innerHTML = json.followers;
+    if (document.getElementById("uptime"))
+      document.getElementById("uptime").innerHTML = days
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+
+
+
+
+
+
+
+
+
     /*const updated = new Date(json.updated_at).toLocaleDateString();
     const created = new Date(json.created_at).toLocaleDateString();
 
@@ -16,11 +45,3 @@ let domain = "spaghetti.nekoweb.org";
     if (document.getElementById("updated"))
       document.getElementById("updated").innerHTML =
         `<em>Updated</em>: ${updated}`;*/
-    if (document.getElementById("views"))
-      document.getElementById("views").innerHTML = json.views;
-    if (document.getElementById("followers"))
-      document.getElementById("followers").innerHTML = json.followers;
-  } catch (error) {
-    console.error(error);
-  }
-})();
