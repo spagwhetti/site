a8bc3f166323ba44348dff667f4d24630b1871d5
spaghetti <spgeti4@gmail.com>
2026-08-22 21:19:33 +0100

    Remove commit message i dont like it

--- public/js/commit.js
+++ public/js/commit.js
@@ -4,7 +4,6 @@ fetch('https://api.github.com/repos/spagwhetti/site/commits?per_page=1')
     .then(res => res.json())
     .then(res => {
         let sha = res[0].sha;
-        document.getElementById('commitMessage').innerText = res[0].commit.message;
         document.getElementById('shortHash').innerText = sha.substring(0, 7);
         document.getElementById('commitLink').href = "https://github.com/spagwhetti/site/commit/" + sha
     });
